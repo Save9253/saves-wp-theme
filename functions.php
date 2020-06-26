31899434da0133bc672379b41cc810f4ef856422
Save <66020327+Save9253@users.noreply.github.com>
2020-06-25 20:13:43 -0400

    linked my styles and js to the theme

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+    wp_enqueue_style('saves-style','/wp-content/themes/saves-wp-theme/saves-style.css');
+    wp_enqueue_script('saves-script','/wp-content/themes/saves-wp-theme/saves-script.js');
+?>
