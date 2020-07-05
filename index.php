8ca62f7cbecfc00aef745482733803b2618d8e19
Save <66020327+Save9253@users.noreply.github.com>
2020-07-05 14:46:28 -0400

    called posts content and page content

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
-<h1>Here should be the posts</h1>
-<hr/>
 <?php
 
-$PageContent = get_post();
+echo "<h2>Test</h2>";
+print_r();#shows what it does
+echo "<hr/>";
 
+#Page Conetent
+$PageContent = get_post();
 echo "<h1>";
 echo apply_filters( 'the_title', $PageContent->post_title );
 echo "</h1>";
 echo apply_filters( 'the_content', $PageContent->post_content );
 echo "<hr/>";
+echo "<h1>Here should be the posts</h1>";
+
+#Blog
+$AllPosts = get_posts();
+if(have_posts()){
+    for($i = 0, $size = count($AllPosts); $i < $size; $i++){
+        echo "<h1>";
+        echo apply_filters( 'the_title', $AllPosts[$i]->post_title);
+        echo "</h1>";
+        echo apply_filters( 'the_content', $AllPosts[$i]->post_content );
+        echo "<br/>";
+    }
+}
 
 ?>
