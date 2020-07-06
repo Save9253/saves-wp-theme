f64bbedce3919d58bb3d4ba361c02aceb393fd4f
Save <66020327+Save9253@users.noreply.github.com>
2020-07-06 13:23:19 -0400

    seach page shows the results and the front page code shortened

--- index.php
+++ index.php
@@ -7,16 +7,18 @@ echo "<h2>Test</h2>";
 echo "<hr/>";
 
 #Page Conetent
-$PageContent = get_post();
-echo "<h1>";
-echo apply_filters( 'the_title', $PageContent->post_title );
-echo "</h1>";
-echo apply_filters( 'the_content', $PageContent->post_content );
-echo "<hr/>";
-echo "<h1>Here should be the posts</h1>";
+if ( have_posts() ) {
+    while ( have_posts() ) : the_post();
+        echo "<h1>";
+        the_title();
+        echo "</h1>";
+        the_content();
+    endwhile;
+};
 
 #Blog
 $AllPosts = get_posts();
+echo "<h1>Here should be the posts</h1>";
 if(have_posts()){
     for($i = 0, $size = count($AllPosts); $i < $size; $i++){
         echo "<h1>";
