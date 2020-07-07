efdce3ddb04f7f1ebaaca7262fb543f9225ee073
Save <66020327+Save9253@users.noreply.github.com>
2020-07-07 18:49:20 -0400

    added fonts, styled the menus

--- functions.php
+++ functions.php
@@ -4,5 +4,5 @@
 
     #Add Nav Menus
     function register_my_menus() {register_nav_menus(array('header-menu' => __( 'Header Menu' )));}
-       add_action( 'init', 'register_my_menus' );
+    add_action( 'init', 'register_my_menus' );
 ?>
