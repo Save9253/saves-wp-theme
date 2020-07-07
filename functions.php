c7054624c0f599d661937f038da27bd15c8294c1
Save <66020327+Save9253@users.noreply.github.com>
2020-07-07 16:56:54 -0400

    relincked the stylesheets

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
-    #Adds CSS and JS
-    wp_enqueue_style('saves-style','/wp-content/themes/saves-wp-theme/saves-style.css');#Add stylesheet
-    wp_enqueue_script('saves-script','/wp-content/themes/saves-wp-theme/saves-script.js');#Add JS
+    wp_enqueue_style('saves-style','/wp-content/themes/saves-wp-theme/assets/saves-style.css');#Add stylesheet
+    wp_enqueue_script('saves-script','/wp-content/themes/saves-wp-theme/assets/saves-script.js');#Add JS
 
     #Add Nav Menus
     function register_my_menus() {register_nav_menus(array('header-menu' => __( 'Header Menu' )));}
