1f5057f90b3c8d471bba4494d0fc282d63fb98e1
Save <66020327+Save9253@users.noreply.github.com>
2020-07-21 16:16:14 -0400

    fixed few more bugs

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -2,14 +2,6 @@
 /**
  * Related Products
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @package     WooCommerce/Templates
  * @version     3.9.0
@@ -37,9 +29,9 @@ if ( $related_products ) : ?>
 
 				<div class="Gridi RelP" style="background-image:url('<?php echo wp_get_attachment_url($related_product->image_id)?>)">
 					<?php
-							if($related_product->is_on_sale()){
-								echo '<div class="slFlshS"><div class="cntr"><p>Sale!</p></div></div>';
-							}
+						if($related_product->is_on_sale()){
+							echo '<div class="slFlshS"><div class="cntr"><p>Sale!</p></div></div>';
+						}
 					?>
 					<div class="RelPCon">
 						<a href="<?php echo get_permalink($related_product->id)?>">
