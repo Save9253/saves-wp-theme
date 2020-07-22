0d9c4b6b818203a0620340303cbded6b5101b41c
Save <66020327+Save9253@users.noreply.github.com>
2020-07-22 15:00:11 -0400

    grid on fornt page doesn't show draft products or outofstock

--- front-page.php
+++ front-page.php
@@ -9,21 +9,19 @@
 <body <?php body_class();?>>
 <?php
     if(function_exists('wc_get_product')){
-        $pdsAll = wc_get_products( $args );
-        function filter_products($prd){
-            if($prd->stock_status == 'instock' && $prd->catalog_visibility == 'visible' && $prd->status == 'publish'){
-                return $prd;
-            }
-        }
-        $pds = array_filter($pdsAll, "filter_products");
-        $pdsC = count($pds) - 1;
+        $pds = wc_get_products(array(
+            'status' => 'publish',
+            'stock_status' => 'instock',
+        ));
+        $pdsC = count($pds);
+        if($pdsC <= 6){$C = 6;}
     };
 ?>
 <div id="bckgr"></div>
 <section id="topGrid">
     <?php
         if(function_exists('wc_get_products')){
-            for($i = 0, $c = 0, $a = 1, $cl=""; $c <= $pdsC; $c++){
+            for($i = 0, $c = 0, $a = 1;$c < $C; $c++){
                 $cl="Gi".$a;
                 if($a > 6){$cl = "hid";};
                 echo '<div class="topGridi Prd WH100 '.$cl.'" style="opacity:1; background-image:url('.wp_get_attachment_url($pds[$i]->image_id).')">';
@@ -39,8 +37,11 @@
                 echo '</p>';
                 echo '<p class="Dis">'.$pds[$i]->short_description.'</p>';
                 echo '</div></a></div>';
-                if($i < $pdsC){$i++;} else {$i = 0;};
-                if($a<=6){echo '<div class="GBck GBck'.$a.'"></div>';$a++;};
+                if($i < $pdsC-1){$i++;} else {$i = 0;};
+                $a++;
+            };
+            for($i = 1;$i <= 6;$i++){
+                echo '<div class="GBck GBck'.$i.'"></div>';
             };
         };
     ?>
@@ -90,4 +91,5 @@
         </div>
     </div>
 </section>
+<?php print_r($product);?>
 <?php get_footer();?>
