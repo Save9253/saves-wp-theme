2f906f05191e63845c35b8a9656dcef32a7770b0
Save <66020327+Save9253@users.noreply.github.com>
2020-07-23 16:53:02 -0400

    front-page mobile wupport

--- assets/Top-Grid.js
+++ assets/Top-Grid.js
@@ -2,25 +2,46 @@
 const GiDis = document.querySelectorAll('.GiDis');
 const GiCont = document.querySelectorAll('.GiA');
 const Prds = document.querySelectorAll('.Prd');
-for(let i = 0;i <= Prds.length-1; i++) {
-    Prds[i].addEventListener('mouseover',()=>{
+if(screen.width > 900){
+    for(let i = 0;i <= Prds.length-1; i++) {
+        Prds[i].addEventListener('mouseover',()=>{
+            GiDis[i].style.height = "87px";
+            GiDis[i].style.padding = "10px";
+            GiCont[i].style.backgroundColor = "rgba(21,18,11,0)";
+        });
+        Prds[i].addEventListener('mouseleave',()=>{
+            GiDis[i].style.height = "0px";
+            GiDis[i].style.padding = "0px";
+            GiCont[i].style.backgroundColor = "rgba(21,18,11,0.5)";
+        });
+    };
+} else {
+    for(let i = 0;i <= Prds.length-1; i++) {
         GiDis[i].style.height = "87px";
         GiDis[i].style.padding = "10px";
         GiCont[i].style.backgroundColor = "rgba(21,18,11,0)";
-    });
-    Prds[i].addEventListener('mouseleave',()=>{
-        GiDis[i].style.height = "0px";
-        GiDis[i].style.padding = "0px";
-        GiCont[i].style.backgroundColor = "rgba(21,18,11,0.5)";
-    });
+    };
 };
 
 //Shwitching Products
-if(Prds.length>6){
+let l = 6;
+if(screen.width < 900 && screen.width > 600){
+    l = 4;
+    document.querySelector('.Gi5').classList.add('hid');
+    document.querySelector('.Gi6').classList.add('hid');
+} else if (screen.width <= 600){
+    l = 1;
+    document.querySelector('.Gi2').classList.add('hid');
+    document.querySelector('.Gi3').classList.add('hid');
+    document.querySelector('.Gi4').classList.add('hid');
+    document.querySelector('.Gi5').classList.add('hid');
+    document.querySelector('.Gi6').classList.add('hid');
+};
+if(Prds.length>l){
     let g = 1;
-    let p = 6;
+    let p = l;
     setInterval(()=>{
-        if(g>6){g=1};
+        if(g>l){g=1};
         if(p>(Prds.length-1)){p=0};
         let Gi = document.querySelector('.Gi'+ g);
         Gi.style.opacity = 0;
@@ -30,11 +51,11 @@ if(Prds.length>6){
             Prds[p].classList.remove('hid');
             Prds[p].classList.add('Gi'+(g));
             Prds[p].style.opacity = 0;
-            console.log('now 1');
-        },750);
+         },750);
         setTimeout(()=>{
-            console.log('now 2');
             Prds[p].style.opacity = 1;
+            console.log('G='+g);
+            console.log('P='+p);
             g++;
             p++;
         },800);
