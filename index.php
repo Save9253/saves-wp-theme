fff1f0d714cfc43a2c3a02f4987fa9f2c78910eb
Save <66020327+Save9253@users.noreply.github.com>
2020-07-26 18:56:45 -0400

    Checkout page styling fixes

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php
-get_header();
+<?php get_header(); ?>
 
+<?php
 if ( have_posts() ) {
     while ( have_posts() ) : the_post();
         the_content();
     endwhile;
 };
-
-get_footer();
 ?>
+
+<?php get_footer();?>
